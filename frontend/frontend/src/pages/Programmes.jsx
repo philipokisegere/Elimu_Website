31e60e55e02196f4aa205449565806878f2fcb05
philipokisegere <147393137+philipokisegere@users.noreply.github.com>
2024-05-24 13:15:22 +0300

    Update Programmes

--- frontend/frontend/src/pages/Programmes.jsx
+++ frontend/frontend/src/pages/Programmes.jsx
@@ -14,14 +14,14 @@ const Programmes = () => {
       setLoading(true);
       const res = await api.get("/getdepartment");
       setDepartments(res.data);
-      console.log(departments);
+     
     } catch (error) {
       console.log("fetch department error: ", error);
     } finally {
       setLoading(false);
     }
     }
-    console.log(departments);
+    
 
   useEffect(() => {
     getDepartments();
@@ -29,26 +29,29 @@ const Programmes = () => {
 
   return (
     <div>
-      <Nav />
-      <div className="mx-auto px-4 py-16">
-        {departments.map((department) => (
-          <div key={department.id} className="text-center mb-12">
-            <h2 className="text-xl font-bold text-blue-700 mb-2">{department.departmentName}</h2>
-                {department.courses.length>0 &&<p>Courses Available </p>}
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-            {department.courses.map((course) => (
+          <Nav />
+          {loading ? <p className="text-center text-blue-500">Loading...</p> :
+              <div className="mx-auto px-4 py-16">
+                  {departments.map((department) => (
+                      <div key={department.id} className="text-center mb-12">
+                          <h2 className="text-xl font-bold text-blue-700 mb-2">{department.departmentName}</h2>
+                          {department.courses.length > 0 && <p>Courses Available </p>}
+                          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+                              {department.courses.map((course) => (
                 
-                <CourseCard
-                  key={course.id}
-                  name={course.courseName}
-                  img={course.img_url}
-                  desc={course.description}
-                />
-            ))}
-                    </div>
-          </div>
-        ))}
-      </div>
+                                  <CourseCard
+                                      key={course.id}
+                                      name={course.courseName}
+                                      img={course.img_url}
+                                      desc={course.description}
+                                  />
+                              ))}
+                          </div>
+                      </div>
+                  ))}
+              </div>
+              
+          }
 
       <Footer />
     </div>
